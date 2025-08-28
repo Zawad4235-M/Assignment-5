293edd9a6669c6ab0776c10a0ccd6690b7fe7b77
Zawad4235-M <zawad.rng@gmail.com>
2025-08-28 14:18:51 +0600

    history

--- js/index.js
+++ js/index.js
@@ -95,3 +95,66 @@ copy_count("copy6","num6")
 copy_count("copy7","num7")
 copy_count("copy8","num8")
 copy_count("copy9","num9")
+
+
+// History
+
+function history_div(id,name,number){
+  document.getElementById(id)
+  .addEventListener("click", function(e){
+    e.preventDefault()
+
+    const newdiv = document.createElement("div")
+    const newdiv2 = document.createElement("div")
+
+    const name_value = document.getElementById(name).innerText
+    const number_value = document.getElementById(number).innerText
+   const now = new Date()
+   const Time = now.toLocaleTimeString()
+
+   const name_el = document.getElementById(name)
+   const number_el = document.getElementById(number)
+
+    const history_name = document.createElement("h4")
+    const history_number = document.createElement("h3")
+    const history_time = document.createElement("span")
+
+    history_name.innerText= name_value
+    history_number.innerText= number_value
+    history_time.innerText = Time
+
+    history_name.className = name_el.className
+    history_name.className = "text-[18px] font-bold"
+    history_number.className =  "text-gray-500"
+
+    newdiv2.appendChild(history_name)
+    newdiv2.appendChild(history_number)
+
+    newdiv.appendChild(newdiv2)
+    newdiv.appendChild(history_time)
+
+    newdiv.className = "div p-2 m-2"
+
+    // coin error
+    const coinElem = document.getElementById("base-coin");
+    const coin_value = parseInt(coinElem.innerText);
+
+    if (coin_value - 20 < 0) {
+        return;
+      }
+    
+
+
+    document.getElementById("history-body").appendChild(newdiv)
+  })
+}
+
+history_div("call-btn1","name1","num1")
+history_div("call-btn2","name2","num2")
+history_div("call-btn3","name3","num3")
+history_div("call-btn4","name4","num4")
+history_div("call-btn5","name5","num5")
+history_div("call-btn6","name6","num6")
+history_div("call-btn7","name7","num7")
+history_div("call-btn8","name8","num8")
+history_div("call-btn9","name9","num9")
